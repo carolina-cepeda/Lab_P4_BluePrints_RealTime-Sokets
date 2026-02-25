1e768804f9d484dffb0a6818135d7ff3a53aacbf
carolina.cepeda-v <carolina.cepeda-v@mail.escuelaing.edu.co>
2026-02-25 09:00:08 -0500

    feat: conexión con stomp actualizada

--- src/lib/stompClient.js
+++ src/lib/stompClient.js
@@ -2,19 +2,28 @@ import { Client } from '@stomp/stompjs'
 // import SockJS from 'sockjs-client' // si quieres fallback
 
 export function createStompClient(baseUrl) {
-  const client = new Client({
-    brokerURL: `${baseUrl.replace(/\/$/,'')}/ws-blueprints`,
-    // webSocketFactory: () => new SockJS(`${baseUrl}/ws-blueprints`),
-    reconnectDelay: 1000,
-    heartbeatIncoming: 10000,
-    heartbeatOutgoing: 10000,
-    onStompError: (f) => console.error('STOMP error', f.headers['message']),
-  })
-  return client
+    const clean = baseUrl.replace(/\/$/, '')
+    const wsUrl = clean.replace(/^http/, 'ws') + '/ws-blueprints'
+
+      const client = new Client({
+        brokerURL: wsUrl,
+
+        reconnectDelay: 1000,
+        heartbeatIncoming: 10000,
+        heartbeatOutgoing: 10000,
+        onStompError: (f) => console.error('STOMP error', f.headers['message']),
+      })
+      return client
 }
 
 export function subscribeBlueprint(client, author, name, onMsg) {
-  return client.subscribe(`/topic/blueprints.${author}.${name}`, (m) => {
-    onMsg(JSON.parse(m.body))
-  })
+
+    const destination = `/topic/blueprints.${author}.${name}`
+
+    const subscription = client.subscribe(destination, (m) => {
+        onMsg(JSON.parse(m.body))
+    })
+
+    return () => subscription.unsubscribe()
 }
+
